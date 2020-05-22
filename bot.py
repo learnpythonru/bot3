3dcb33a0ba2d4acd1f5217f9b36037836e963ed2
Mikhael Korneev <korneevm@gmail.com>
2020-05-22 13:58:05 +0300

    Learn Python Bots track 3 code

---
+++ bot.py
@@ -0,0 +1,30 @@
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from handlers import (greet_user, guess_number, send_cat_picture, user_coordinates,
+                        talk_to_me)
+import settings
+
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_number))
+    dp.add_handler(CommandHandler("cat", send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'), send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.location, user_coordinates))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
